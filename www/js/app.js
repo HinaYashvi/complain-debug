1a2d0d971c1a1d2a3576d91ebcaaab8d27091fc7
unknown <hina.sparmar@gmail.com>
2018-12-27 16:32:15 +0530

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -94,7 +94,7 @@ function getPosition() {
    }; 
    var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
-   function onSuccess(position) {
+   function onSuccess(position) { 
       alert('Latitude: '          + position.coords.latitude          + '\n' +
          'Longitude: '         + position.coords.longitude         + '\n' +
          'Altitude: '          + position.coords.altitude          + '\n' +
