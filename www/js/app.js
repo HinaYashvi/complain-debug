fdbea1bfb486f1d507f139f4d2c1c2a2ac9d2e27
HinaYashvi <hina.parmar@staroneweb.co.in>
2018-12-31 17:48:08 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -184,13 +184,35 @@ function onFail(message) {
   alert('Failed because: ' + message);
 }
 function upload(){   
-  var latLong = navigator.geolocation.getCurrentPosition(function(position){
+  /*var latLong = navigator.geolocation.getCurrentPosition(function(position){
     var longitude = position.coords.longitude;
     var latitude = position.coords.latitude;
   }, function (error){
     alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
   },{ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }); 
-  alert("latitude :: "+latitude+" longitude ::"+longitude);
+  alert("latitude :: "+latitude+" longitude ::"+longitude);*/
+  var options = {
+      maximumAge: 3000, 
+      timeout: 5000, 
+      enableHighAccuracy: true
+   },
+   var watchID = navigator.geolocation.getCurrentPosition(onSuccessLoc, onErrorLoc, options);
+
+   function onSuccessLoc(position) {       
+         var longitude = position.coords.longitude;
+         var latitude = position.coords.latitude;
+         alert("latitude :: "+latitude+" longitude ::"+longitude); 
+   };
+   function onErrorLoc(error) {
+      alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+   }
+  /*var latLong = navigator.geolocation.getCurrentPosition(function(position){
+    var longitude = position.coords.longitude;
+    var latitude = position.coords.latitude;
+  }, function (error){
+    alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+  },{ maximumAge: 3000, timeout: 5000, enableHighAccuracy: true }); */
+  alert("latitude :: "+latitude+" longitude ::"+longitude); 
 	
   var img = document.getElementById('image'); 
   app.dialog.preloader('Uploading....');
